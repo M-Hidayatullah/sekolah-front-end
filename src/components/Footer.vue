418fe08805f7608dc6ce0d0b3b58f25ac7762f96
M-Hidayatullah <hidayatullahblk@gmail.com>
2022-02-03 13:49:20 +0800

    push projects

---
+++ src/components/Footer.vue
@@ -0,0 +1,68 @@
+<template>
+    <footer>
+        <div class="container-fluid" style="background: white;">
+            <div class="row p-4">
+                <div class="col-md-4">
+                    <h5>TENTANG</h5>
+                    <hr>
+                    <p>
+                        MTS ALIMUDDIN RIDHO adalah salah satu satuan pendidikan dengan jenjang MTs di Ubung, Kec. Jonggat, Kab. Lombok Tengah, Nusa Tenggara Barat. Dalam menjalankan kegiatannya, MTS ALIMUDDIN RIDHO berada di bawah naungan Kementerian Agama.
+                    </p>
+                </div>
+                <div class="col-md-4">
+                    <h5>TAGS</h5>
+                    <hr>
+                    <div v-if="tags.length > 0">
+                        <router-link :to="{name: 'detail_tag', params: { slug: tag.slug }}" v-for="tag in tags" :key="tag.id" class="btn btn-sm btn-outline-secondary mb-2 me-2">{{ tag.name.toUpperCase() }}</router-link>
+                    </div>
+                    <div v-else class="text-center">
+                        <ListLoader/>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <h5>KONTAK</h5>
+                    <hr>
+                    <p>
+                        <i class="fa fa-map-marker" aria-hidden="true"></i> JL. IHDINASSIRAATHALMUSTAQIIM, Ubung Kecamatan Jonggat Kabupaten Lombok Tengah Prov. Nusa Tenggara Barat.
+                        <i class="fas fa-phone"></i> +62878-8567-80210
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid bg-dark">
+            <div class="row p-3">
+                <div class="text-center text-white font-weight-bold">
+                    Copyright © 2022 MTS ALIMUDDIN RIDHO. All rights reserved.
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+    //import content loader
+    import {
+        ListLoader
+    } from 'vue-content-loader'
+
+    //import axios
+    import axios from 'axios'
+
+    export default {
+        name: 'Footer',
+        components: {
+            //loader component
+            ListLoader
+        },
+        data() {
+            return {
+                tags: []
+            }
+        },
+        created() {
+            axios.get('/api/tag').then(response => {
+                this.tags = response.data.data.data
+            });
+        }
+    }
+</script>
---
+++ src/views/contact/Index.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="post-index">
+        <!-- header -->
+        <Header />
+        <!-- end header -->
+
+        <!-- main content -->
+        <header class="pt-5 border-bottom bg-light">
+            <div class="container pt-md-1 pb-md-1">
+                <h1 class="bd-title mt-4 font-weight-bold"><i class="fa fa-phone" aria-hidden="true"></i> KONTAK</h1>
+                <p class="bd-lead">Kontak MTS ALIMUDDIN RIDHO.</p>
+            </div>
+        </header>
+
+        <!-- breadcrumb -->
+        <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><router-link :to="{ name: 'home' }" class="text-decoration-none"><i class="fa fa-home"></i> Home</router-link></li>
+            <li class="breadcrumb-item"><router-link :to="{ name: 'contact' }" class="text-decoration-none"><i class="fa fa-phone"></i> Kontak</router-link></li>
+        </ol>
+        </nav>
+        <!-- end breadcrumb -->
+
+        <div class="container-fluid mt-5 mb-5">
+
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="card border-0 shadow-sm rounded">
+                        <div class="card-body">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31553.95098784489!2d116.18414353955073!3d-8.66834209999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dcdbb73edf61599%3A0x4f7733124397e67c!2sMTS%20ALIMUDIN%20RIDHO!5e0!3m2!1sen!2sid!4v1642514916121!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="card border-0 shadow-sm rounded">
+                        <div class="card-body">
+                            <h3>KONTAK KAMI</h3>
+                            <hr>
+                            <p>
+                                <i class="fa fa-map-marker" aria-hidden="true"></i> JL. IHDINASSIRAATHALMUSTAQIIM, Ubung Kecamatan Jonggat Kabupaten Lombok Tengah Prov. Nusa Tenggara Barat.<br>
+                                <i class="fas fa-phone"></i> +62878-8567-80210<br>
+
+                                <i class="fas fa-envelope"></i> hidayatullahblk@gmail.com
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- main content -->
+
+        <!-- footer -->
+        <Footer />
+        <!-- end footer -->
+    </div>
+</template>
+
+<script>
+    //import component
+    import Header from "@/components/Header"
+    import Footer from "@/components/Footer"
+
+
+    export default {
+        name: 'ContactIndex',
+        components: {
+            Header,
+            Footer
+        }
+    }
+</script>
